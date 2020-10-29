018f15539074960a1e6f2691aab1e9365426a48f
a.tulubieva <71927278+tuluqeva@users.noreply.github.com>
2020-10-30 00:01:39 +0400

    Update solution_7.py

--- task_7/solution_7.py
+++ task_7/solution_7.py
@@ -1,1 +1,13 @@
-# your code here
+a = input()
+start = ''
+end = ''
+pal = True
+g = 0
+for h in 'H'*len(a):
+    end = a[g]+end
+    if not end in a:
+        pal = False
+        g += 1
+    else:
+        g += 1
+print('is_palindrom',pal)
