508b837b53f2242bec3cc7a586141c9b3a67fc74
a.tulubieva <71927278+tuluqeva@users.noreply.github.com>
2020-10-30 00:36:50 +0400

    Update solution_3_1.py

--- task_3/solution_3_1.py
+++ task_3/solution_3_1.py
@@ -1,4 +1,20 @@
-# your code here
-user_input = input()
-# your code here
-print(alphabet)
+m = input()
+i = ''
+a = ''
+num = 0
+g = 0
+k = 1
+for M in 'M'*(len(m)-1):
+    k = 1
+    for J in 'J'*(len(m)-num-1):
+        if m[num] == m[num+k]:
+            i = i+str(num)
+            k += 1
+        else:
+            k += 1
+    num += 1
+for w in 'W'*len(m):
+    if not str(g) in i:
+        a = a + m[g] 
+    g += 1
+print(a)
